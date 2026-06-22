b55035afd53abcd65f58e3c5b77c11bec41f88e9
Psynox-lang <utkarshmumbai5@gmail.com>
2026-06-22 16:50:53 +0530

    Use Google credentials env variable

--- Backend/services/sheets_service.py
+++ Backend/services/sheets_service.py
@@ -1,13 +1,19 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import json
+import os
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = Credentials.from_service_account_file(
-    "credentials.json",
+creds_dict = json.loads(
+    os.environ["GOOGLE_CREDENTIALS"]
+)
+
+creds = Credentials.from_service_account_info(
+    creds_dict,
     scopes=SCOPES,
 )
 
